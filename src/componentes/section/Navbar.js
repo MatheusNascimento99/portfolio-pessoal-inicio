b863f21a59c5fe447228c7df639875b04f0c7360
MatheusNascimento99 <matheuscrnascimento@gmail.com>
2023-10-04 17:00:07 -0300

    Atualização

--- src/componentes/section/Navbar.js
+++ src/componentes/section/Navbar.js
@@ -1,7 +1,19 @@
+import styles from './Navbar.module.css'
+import { FaLinkedin, FaGithub } from 'react-icons/fa';
+import Nav from 'react-bootstrap/Nav'
+
 function Navbar(){
     return (
-        <div>
-            <h1>aqui fica o nav bar</h1>
+        <div className ={styles.navbar}>
+            <ul>
+                <li><Nav.Link href='#Presentation'>Apresentação</Nav.Link></li>
+                <li><Nav.Link href='#Skills'>Habilidades</Nav.Link></li>
+                <li><Nav.Link href='#Projects'>Projetos</Nav.Link></li>
+            </ul>
+            <ul>
+                <li><a target="_blank" href='https://github.com/MatheusNascimento99'><FaGithub size={45}/></a></li>
+                <li><a target="_blank"  href='https://www.linkedin.com/in/matheus-nascimento-106a00226/'><FaLinkedin size={45}/></a></li>
+            </ul>
         </div>
     )
 }
